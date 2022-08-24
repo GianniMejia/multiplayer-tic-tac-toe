536dcab342bd072e3f9a6ee140063350c81cc69e
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-08-24 00:09:08 -0700

    setup react router

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,20 @@
+import Signup from "./signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <header></header>
+        <main>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+        <footer></footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/signup.js
@@ -0,0 +1,13 @@
+function Signup() {
+  return (
+    <div>
+      <form>
+        <label>
+          Username <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
